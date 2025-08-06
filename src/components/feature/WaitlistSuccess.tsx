328b122a741e99c0fe0e7873f4d5662450cd75a1
devhnry <devthenry@gmail.com>
2025-08-06 16:24:49 +0100

    chore: updated success response for Waitlist

--- src/components/feature/WaitlistSuccess.tsx
+++ src/components/feature/WaitlistSuccess.tsx
@@ -1,8 +1,13 @@
+import {CheckSquare} from "lucide-react";
+
 export default function WaitlistSuccess() {
     return (
-        <div className="w-full max-w-md mx-auto text-center space-y-4">
-            <h2 className="text-2xl font-bold text-green-600">You're on the list! ✅</h2>
-            <p className="text-gray-600">
+        <div className="w-fit mx-auto my-4 text-center space-y-4 shadow-lg shadow-black/10 rounded-md p-4 relative">
+            <h2 className="flex items-center w-fit mx-auto gap-2 text-xl md:text-2xl leading-tight tracking-tight font-bold text-primary">
+                You're on the list!
+                <span><CheckSquare className={`size-5 text-primary-dark`} /></span>
+            </h2>
+            <p className="text-black/70 font-bold max-w-[350px] w-full">
                 We'll send you updates about Divyd as we roll out features. Stay tuned!
             </p>
         </div>
